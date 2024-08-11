ab91b775a12bd0940bf2c48554debd01fc56a1f7
eder24m3za <egmr.49@gmail.com>
2024-08-10 22:16:43 -0600

    Asignar hora de llegada y finalizar visita Android

--- app/src/main/java/com/example/transmisiondigital/VisitActivity.java
+++ app/src/main/java/com/example/transmisiondigital/VisitActivity.java
@@ -2,6 +2,7 @@ package com.example.transmisiondigital;
 
 import static com.example.transmisiondigital.globalVariables.Conexion.URL;
 
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -26,25 +27,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.imageview.ShapeableImageView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.Locale;
 
 public class VisitActivity extends AppCompatActivity {
 
-    private String idVisit;
+    private String idVisit, horaLlegada, fechaHoraSolicitudStr, motivo, direccion, tecnicoId, clienteId, sucursalId;
     private SharedPreferences sharedPreferences;
-    private TextView textViewFolio, textViewDate, textViewHour, textViewAddress, textViewCustomer;
+    private TextView textViewFolio, textViewDate, textViewHour, textViewAddress, textViewCustomer, textViewReason;
     private TextView textViewTechnical, textViewApplicant, textViewPosition, textViewStatus, textViewEntryTime;
-    private Button buttonSave, buttonAttend;
+    private Button buttonSave, buttonAttend, buttonSetHour;
     private Spinner spinnerStatus;
     private ArrayAdapter<String> adapter;
 
@@ -63,6 +70,7 @@ public class VisitActivity extends AppCompatActivity {
 
     public void init() {
         textViewFolio = findViewById(R.id.textViewFolio);
+        textViewReason = findViewById(R.id.textViewReason);
         textViewDate = findViewById(R.id.textViewDate);
         textViewHour = findViewById(R.id.textViewHour);
         textViewAddress = findViewById(R.id.textViewAddress);
@@ -75,13 +83,15 @@ public class VisitActivity extends AppCompatActivity {
         buttonSave = findViewById(R.id.buttonSave);
         spinnerStatus = findViewById(R.id.spinnerStatus);
         buttonAttend = findViewById(R.id.buttonAttend);
-        buttonAttend.setVisibility(View.GONE);
+        buttonSetHour = findViewById(R.id.buttonSetHour);
 
         if (sharedPreferences.getString("rol", null).equals("Técnico")) {
             spinnerStatus.setVisibility(View.GONE);
             buttonSave.setVisibility(View.GONE);
             buttonAttend.setVisibility(View.VISIBLE);
+            buttonSetHour.setVisibility(View.VISIBLE);
         }
+
         header();
         String[] items = {"Autorizar", "Finalizar", "Cancelar"};
 
@@ -95,7 +105,7 @@ public class VisitActivity extends AppCompatActivity {
         requestApi();
         buttonSave();
         setButtonAttend();
-
+        setHour();
     }
 
     public void header() {
@@ -121,7 +131,7 @@ public class VisitActivity extends AppCompatActivity {
                 String clienteNombre = data.getJSONObject("cliente").getString("nombre");
                 String tecnicoNombre = data.getJSONObject("tecnico").getString("nombre");
 
-                String fechaHoraSolicitudStr = data.optString("fechaHoraSolicitud", "");
+                fechaHoraSolicitudStr = data.optString("fechaHoraSolicitud", "");
                 String fechaHoraLlegadaStr = data.optString("fechaHoraLlegada", "");
                 SimpleDateFormat formatoOriginal = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                 SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd");
@@ -143,12 +153,17 @@ public class VisitActivity extends AppCompatActivity {
                 String fechaSolicitud = (fechaHoraSolicitud != null) ? formatoFecha.format(fechaHoraSolicitud) : "";
                 String horaSolicitud = (fechaHoraSolicitud != null) ? formatoHora.format(fechaHoraSolicitud) : "";
                 String horaLlegada = (fechaHoraLlegada != null) ? formatoHora.format(fechaHoraLlegada) : "";
-
+                motivo = data.optString("motivo", "");
+                direccion = data.optString("direccion", "");
+                tecnicoId = data.optString("tecnico_id", "");
+                clienteId = data.optString("cliente_id", "");
+                sucursalId = data.optString("sucursal_id", "");
 
                 textViewFolio.setText("Folio: " + data.getString("id"));
+                textViewReason.setText("Motivo: " + motivo);
                 textViewDate.setText("Fecha: " + fechaSolicitud);
                 textViewHour.setText("Hora: " + horaSolicitud);
-                textViewAddress.setText("Dirección: " + data.getString("direccion"));
+                textViewAddress.setText("Dirección: " + direccion);
                 textViewCustomer.setText("Cliente: " + clienteNombre);
                 textViewTechnical.setText("Tecnico: " + tecnicoNombre);
                 textViewStatus.setText("Estatus: " + data.getString("estatus"));
@@ -218,16 +233,26 @@ public class VisitActivity extends AppCompatActivity {
 
     public void setButtonAttend() {
         buttonAttend.setOnClickListener(v -> {
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PATCH, URL + "visitas/finalizar/" + idVisit, null, response -> {
-                try {
-                    String message = response.getString("msg");
-                    Toast.makeText(VisitActivity.this, message, Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(this, VisitsActivity.class);
-                    startActivity(intent);
-                    finish();
-                } catch (JSONException e) {
-                    e.printStackTrace();
+            String entryTimeText = textViewEntryTime.getText().toString();
+            if (entryTimeText.contains("Hora de llegada: ")) {
+                Log.i("", entryTimeText);
+                String[] parts = entryTimeText.split("Hora de llegada: ");
+                if (parts.length > 1 && !parts[1].trim().isEmpty()) {
+                    horaLlegada = parts[1];
+                } else {
+                    horaLlegada = "";
                 }
+            } else {
+                horaLlegada = "";
+            }
+
+            if (horaLlegada.isEmpty()) {
+                Toast.makeText(VisitActivity.this, "Debe seleccionar una hora de llegada", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PATCH, URL + "visitas/finalizar/" + idVisit, null, response -> {
+                requestApiupdate();
             }, error -> {
                 try {
                     String responseBody = new String(error.networkResponse.data, "utf-8");
@@ -242,6 +267,109 @@ public class VisitActivity extends AppCompatActivity {
             RequestQueue requestQueue = Volley.newRequestQueue(VisitActivity.this);
             requestQueue.add(jsonObjectRequest);
         });
+    }
+
+    public void setHour() {
+        buttonSetHour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the current time
+                Calendar calendar = Calendar.getInstance();
+                int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+                int currentMinute = calendar.get(Calendar.MINUTE);
+
+                // Subtract one hour from the current time
+                calendar.add(Calendar.HOUR_OF_DAY, -1);
+                int defaultHour = calendar.get(Calendar.HOUR_OF_DAY);
+                int defaultMinute = calendar.get(Calendar.MINUTE);
+
+                // Open a time picker dialog with the default time set to one hour less than the current time
+                TimePickerDialog timePickerDialog = new TimePickerDialog(VisitActivity.this, (view, hourOfDay, minute) -> {
+                    // Set the selected time to the textViewHour
+                    String selectedTime = String.format(Locale.getDefault(), "%02d:%02d", hourOfDay, minute);
+                    Log.d("HourSelected", "onClick: " + selectedTime);
+                    textViewEntryTime.setText("Hora de llegada: " + selectedTime);
+                }, defaultHour, defaultMinute, true);
+
+                timePickerDialog.show();
+            }
+        });
+    }
+
+    public void requestApiupdate() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        String currentDate = dateFormat.format(calendar.getTime());
+
+        // Combine current date with horaLlegada and add seconds as 00
+        String formattedDateTime = currentDate + " " + horaLlegada + ":00";
+        horaLlegada = formattedDateTime;
+
+        SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        String fechaHoraSalida = dateTimeFormat.format(calendar.getTime());
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("motivo", motivo);
+            jsonObject.put("direccion", direccion);
+            jsonObject.put("tecnico_id", tecnicoId);
+            jsonObject.put("cliente_id", clienteId);
+            jsonObject.put("sucursal_id", sucursalId);
+            jsonObject.put("fechaHoraSolicitud", fechaHoraSolicitudStr);
+            jsonObject.put("fechaHoraLlegada", horaLlegada);
+            jsonObject.put("fechaHoraSalida", fechaHoraSalida);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, URL + "visitas/" + idVisit, jsonObject, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                String message = null;
+                try {
+                    message = response.getString("msg");
+                    Toast.makeText(VisitActivity.this, message, Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(VisitActivity.this, VisitsActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                try {
+                    Log.e("Error", "onErrorResponse: " + error);
+                    String responseBody = new String(error.networkResponse.data, "utf-8");
+                    JSONObject data = new JSONObject(responseBody);
+
+                    if (data.has("errors")) {
+                        JSONObject errors = data.getJSONObject("errors");
+                        StringBuilder errorMessage = new StringBuilder();
+
+                        for (Iterator<String> it = errors.keys(); it.hasNext(); ) {
+                            String key = it.next();
+                            JSONArray errorMessages = errors.getJSONArray(key);
+                            for (int i = 0; i < errorMessages.length(); i++) {
+                                String errorMsg = errorMessages.getString(i);
+                                errorMessage.append(errorMsg).append("\n");
+                                Log.e("ErrorDetail", key + ": " + errorMsg);
+                            }
+                        }
+
+                        Toast.makeText(VisitActivity.this, errorMessage.toString().trim(), Toast.LENGTH_SHORT).show();
+                    } else {
+                        String message = data.getString("message");
+                        Log.e("ErrorMessage", message);
+                        Toast.makeText(VisitActivity.this, message, Toast.LENGTH_SHORT).show();
+                    }
+                } catch (UnsupportedEncodingException | JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(VisitActivity.this);
+        requestQueue.add(jsonObjectRequest);
     }
 
     public void footer() {
